4359e1b3517ee30a34ec994e3526f0e42bdd337a
DepressyImpressy <thelasteclipse557@gmail.com>
2026-06-13 19:51:19 -0500

    v.05
    +Added a collapsible gallery separated in categories, adding a few of the first images and formatting them into a table of four columns

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ function myFunction() {
 
 const button = document.getElementById("theme-toggle");
 
+function closeAllGalleries() {
+    const galleries = document.querySelectorAll('.full-gallery');
+    galleries.forEach(g => g.style.display = 'none');
+}
+
 button.addEventListener("click", function(){
     console.log("The button was clicked!");
     
@@ -13,32 +18,59 @@ button.addEventListener("click", function(){
     
 });
 
-document.getElementById('fullGalleryButton').addEventListener('click', function(){
-    
+document.getElementById('fullGalleryButton').addEventListener('click', function () {
+
     const buttonSpread = document.getElementById('gCategoryButtons');
 
     //toggles button visiblity
-    if(buttonSpread.style.display === 'none'){
+    if (buttonSpread.style.display === 'none') {
         buttonSpread.style.display = 'block';
         this.textContent = 'Close Gallery'; //changes button text 
     }
-    else{
+    else{ 
         buttonSpread.style.display = 'none';
         this.textContent = 'Full Moment Gallery';
+
+        closeAllGalleries();
     }
 });
 
-//full gallery buttons
+//accesses the Dragon's dogma full
 document.getElementById('ddB').addEventListener('click', function () {
-    console.log("Dragon's Dogma!");
+    closeAllGalleries();
+    const ddG = document.getElementById('DragonsDogmaGallery');
+    const elements = document.getElementsByClassName("column");
 
-    //redirect the page to a new gallery of dragon's dogma moments clips and images
+    ddG.style.display = 'block';
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "25%";
+    }
 });
 
 document.getElementById('mB').addEventListener('click', function () {
     console.log("Metroid!");
+    closeAllGalleries();
+    const mG = document.getElementById('MetroidGallery');
+    const elements = document.getElementsByClassName("column");
+
+    mG.style.display = 'block';
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "25%";
+    }
 });
 
 document.getElementById('mhB').addEventListener('click', function () {
     console.log("Monster Hunter!");
+});
+
+document.getElementById('reB').addEventListener('click', function () {
+    console.log("Resident Evil!")
+});
+
+document.getElementById('sotfB').addEventListener('click', function () {
+    console.log("Sons of the forest!");
+});
+
+document.getElementById('xB').addEventListener('click', function () {
+    console.log("Xenoblade!");
 });
