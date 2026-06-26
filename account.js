6eaf20ab15cd7a18dec532f6fb9ad5b5f8e64145
DepressyImpressy <thelasteclipse557@gmail.com>
2026-06-26 14:12:55 -0500

    v0.1.2 Uploaded files missing
    +uploaded the two files that should be attached to the account html doc
    +have a few missed breaks that need to be fixed urgently

---
+++ account.js
@@ -0,0 +1,45 @@
+﻿//edit button logic 
+const editInfo = document.getElementById("editInfo");
+const editBtn = document.getElementById("editBtn");
+const profileInfo = document.getElementById("profileInfo");
+//make edit button disappear and the fields that were concrete become input fields
+
+if (editBtn && editInfo && profileInfo) {
+    editBtn.addEventListener("click", () => {
+        editInfo.classList.toggle("hidden");
+        profileInfo.classList.add("hidden");
+        editBtn.textContent = ("Save Changes")
+    });
+}
+
+//button click logic
+const pronounBtn = document.getElementById("pronounBtn");
+const pronounMenu = document.getElementById("pronounMenu");
+
+if (pronounBtn && pronounMenu) {
+    pronounBtn.addEventListener("click", () => {
+        pronounMenu.classList.toggle("hidden");
+    })
+}
+
+//user choice
+const options = document.querySelectorAll(".dropdown-option");
+
+options.forEach(option => () => {
+    option.addEventListener("click", () => {
+        const value = option.dataset.value;
+
+        localStorage.setItem("pronouns", value);
+
+        pronounBtn.textContent = `${value}`;
+
+        pronounMenu.classList.add("hidden");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const saved = localStorage.getItem("pronouns");
+    if (saved) {
+        pronounBtn.textContent = `${saved}`;
+    }
+})
